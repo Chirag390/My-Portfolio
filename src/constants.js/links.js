bdcace4f62f2e42d0b555d06621b44096bab2985
chirag goyal <chiraggoyalcg640@gmail.com>
2025-08-25 14:40:52 +0530

    Initial commit - My Portfolio

---
+++ src/constants.js/links.js
@@ -0,0 +1,13 @@
+// social accounts
+export const gmailLink = "mailto:chiraggoyalcg640@gmail.com"
+export const linkedinLink = "https://www.linkedin.com/in/chirag-goyal-cg640/"
+export const instagramLink = "#"
+export const githubLink = "https://github.com/Chirag390"
+export const discordLink = ""
+// projects links
+export const projectsInfo = {
+    JobSpot: {
+        visit: "https://jobspot-e2c6.onrender.com/",
+        github: "https://github.com/Chirag390/jobSpot"
+    },
+}
